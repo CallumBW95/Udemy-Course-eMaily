85b123803b7f6dc355e7d379af2251f6d601ebd9
Callum Bridgford-Whittick <callumbw@gmail.com>
2018-05-08 22:35:07 +0100

    start building the Survey Form

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import SurveyFrom from "./SurveyForm";
+
+class SurveyNew extends Component {
+  render() {
+    return <SurveyFrom />;
+  }
+}
+
+export default SurveyNew;
